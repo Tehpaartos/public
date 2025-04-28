c019454e4ac001b5bada6e42ebe385c17297cd8b
Tehpaartos <chris.warren.melb@gmail.com>
2025-04-28 23:19:37 +1000

    Removed file and updated osdcloud

--- codux.osdcloud.com.ps1
+++ codux.osdcloud.com.ps1
@@ -1,10 +1,10 @@
 <#PSScriptInfo
-Script should be executed in a Command Prompt using the following command
+Script should be executed in a Command Prompt within WinPE using the following command
 powershell Invoke-Expression -Command (Invoke-RestMethod -Uri osdcloud.codoit.com.au)
 This is abbreviated as
 powershell iex (irm osdcloud.codoit.com.au)
 #>
-#Requires -RunAsAdministrator
+
 <#
 .SYNOPSIS
     PowerShell Script which supports the OSDCloud environment
@@ -17,6 +17,7 @@ powershell iex (irm osdcloud.codoit.com.au)
 .EXAMPLE
     powershell iex (irm osdcloud.codoit.com.au)
 #>
+
 [CmdletBinding()]
 param()
 $ScriptName = 'osdcloud.codoit.com.au'
@@ -66,13 +67,6 @@ if ($WindowsPhase -eq 'WinPE') {
     Write-Host -ForegroundColor Cyan "Start-OSDCloud, Start-OSDCloudGUI, or Start-OSDCloudAzure, can be run in the new PowerShell window"
     
     #Configure OSDCloudGUI
-    # Import-module OSD -Force
-    # $OSDModuleResource.StartOSDCloudGUI.BrandName = 'Codux'
-
-    #Start OSDCloudGUI
-    # Start-Process powershell -ArgumentList "-Command Start-OSDCloudGUI"
-
-
     $configureOSDCloudGUI = @"
 \Import-Module OSD -Force
 `$OSDModuleResource.StartOSDCloudGUI.BrandName = 'Codux'
@@ -91,69 +85,10 @@ if ($WindowsPhase -eq 'WinPE') {
 Start-OSDCloudGUI
 "@
 
-Start-Process powershell "-Command", $configureOSDCloudGUI
+    #Start OSDCloudGUI
+    Start-Process powershell "-Command", $configureOSDCloudGUI
     
     #Stop the startup Transcript.  OSDCloud will create its own
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region Specialize
-if ($WindowsPhase -eq 'Specialize') {
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region AuditMode
-if ($WindowsPhase -eq 'AuditMode') {
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region OOBE
-if ($WindowsPhase -eq 'OOBE') {
-    #Remove built-in apps before winget upgrade
-    RemoveAppx BingNews, BingWeather, GamingApp, GetHelp, Getstarted, MicrosoftOfficeHub, Office.OneNote, MicrosoftSolitaireCollection, MicrosoftStickyNotes, People, PowerAutomateDesktop, ScreenSketch, SkypeApp, Todos, WindowsAlarms, windowscommunicationsapps, WindowsFeedbackHub, WindowsMaps, Xbox.TCUI, XboxGameOverlay, XboxGamingOverlay, XboxIdentityProvider, XboxSpeechToTextOverlay, YourPhone, ZuneMusic, ZuneVideo, XboxApp, MixedReality.Portal, Microsoft3DViewer, Wallet, MSPaint, 549981C3F5F10,Windows.DevHome
-    
-    #Windows Feature Disablement
-    $winBuild = [System.Environment]::OSVersion.Version.Build
-    $settings = @()
-    
-    if ($winBuild -like "19*") {
-        # Windows 10 settings
-        $settings = @(
-            @{Path="HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"; Name="EnableFeeds"; Value=0; Type="DWORD"}
-        )
-    }
-    elseif ($winBuild -like "22*") {
-        # Windows 11 settings
-        $settings = @(
-            @{Path="HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Chat"; Name="ChatIcon"; Value=3; Type="DWORD"},
-            @{Path="HKLM:\SOFTWARE\Policies\Microsoft\Dsh"; Name="AllowNewsAndInterests"; Value=0; Type="DWORD"}
-        )
-    }
-    
-    foreach ($s in $settings) {
-        New-Item -Path $s.Path -Force -ErrorAction SilentlyContinue | Out-Null
-        Set-ItemProperty -Path $s.Path -Name $s.Name -Value $s.Value -Type $s.Type
-        Write-Host "Set '$($s.Name)' to '$($s.Value)' in '$($s.Path)'"
-    }
-    
-    #Load everything needed to run AutoPilot
-    osdcloud-StartOOBE -Autopilot -InstallWinGet -WinGetUpgrade
-
-    #Install standard apps
-    winget install --id Google.Chrome --silent --scope machine --accept-source-agreements --accept-package-agreements
-    winget install --id 7zip.7zip --silent --scope machine --accept-source-agreements --accept-package-agreements
-    winget install --id Adobe.Acrobat.Reader.64-bit --silent --scope machine --accept-source-agreements --accept-package-agreements
-    
-    $null = Stop-Transcript -ErrorAction Ignore
-}
-#endregion
-
-#region Windows
-if ($WindowsPhase -eq 'Windows') {
-    #Load OSD and Azure stuff
     $null = Stop-Transcript -ErrorAction Ignore
 }
 #endregion
